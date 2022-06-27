db958aa4f10d68328c203821981a1dc50849de3c
jinyong <zhaqh146@naver.com>
2022-06-27 21:30:11 +0900

    2022-06-27

--- level3/경주로 건설.py
+++ level3/경주로 건설.py
@@ -4,8 +4,7 @@ def solution(board):
     dr = [-1, 0, 1, 0]
     dc = [0, 1, 0, -1]
     queue = [(0, 0, 0, 0, 0)]
-    check = [[987654321] * n for _ in range(n)]
-    check[0][0] = 0
+    check = [[[987654321] * 4 for _ in range(n)] for _ in range(n)]
     idx = 0
     length = 1
 
@@ -29,11 +28,18 @@ def solution(board):
             new_direction = 1 if nr == r else -1
             new_cost = cost + (600 if new_direction * direction == -1 else 100)
 
-            if check[nr][nc] < new_cost:
+            if min(check[nr][nc]) + 500 < new_cost:
+                # 가려는 곳까지의 비용이 4방향 비용의 최솟값에 500을 더한 값보다 큰 경우 그 곳은 더 어느 방향으로도 더 볼 필요 없다
+                continue
+
+            if check[nr][nc][(di + i) % 4] < new_cost:
+                # (n-1, r-2)를 왼쪽에서부터 1000원으로 왔고, (n-1, r-2)를 위쪽에서부터 700원으로 온 경우
+                # 위쪽에서 온 경우가 최종적으로는 더 비싼 비용이 필요하게 된다
+                # 따라서, 어느 방향에서 왔는지도 추가로 확인해서 비교해야 한다.
                 continue
 
             queue.append((nr, nc, new_cost, new_direction, i))
-            check[nr][nc] = new_cost
+            check[nr][nc][(di + i) % 4] = new_cost
             length += 1
 
     return answer
