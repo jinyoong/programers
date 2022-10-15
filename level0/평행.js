068a41aaee984a4987bddd1dcdbbc0743960ada2
jinyong <zhaqh146@naver.com>
2022-10-15 19:01:59 +0900

    2022-10-15

---
+++ level0/평행.js
@@ -0,0 +1,16 @@
+function solution(dots) {
+  var answer = 0;
+  let possibleArray = [[1, 2, 3, 4], [1, 3, 2, 4], [1, 4, 2, 3]];
+  
+  possibleArray.map((possible) => {
+    let inc1;
+    let inc2;
+    let first = possible[0]
+    let second = possible[1]
+    let third = possible[2]
+    let last = possible[3]
+
+  })
+
+  return answer;
+}
