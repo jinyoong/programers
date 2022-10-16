9de1a6090fc3d31126348db37539966dd3b2c111
jinyong <zhaqh146@naver.com>
2022-10-16 17:54:53 +0900

    2022-10-16

--- level0/평행.js
+++ level0/평행.js
@@ -1,6 +1,6 @@
 function solution(dots) {
   var answer = 0;
-  let possibleArray = [[1, 2, 3, 4], [1, 3, 2, 4], [1, 4, 2, 3]];
+  let possibleArray = [[0, 1, 2, 3], [0, 2, 1, 3], [0, 3, 1, 2]];
   
   possibleArray.map((possible) => {
     let inc1;
@@ -10,7 +10,25 @@ function solution(dots) {
     let third = possible[2]
     let last = possible[3]
 
+    if (dots[first][0] === dots[second][0]) {
+      if (dots[third[0] === dots[last][0] ]) {
+        answer += 1
+      }
+    } else {
+      inc1 = (dots[first][1] - dots[second][1]) / (dots[first][0] - dots[second][0])
+      inc2 = (dots[third][1] - dots[last][1]) / (dots[third][0] - dots[last][0])
+
+      if (inc1 === inc2) {
+        console.log("같음")
+        answer += 1
+      }
+    }
+
   })
 
-  return answer;
+  if (answer > 0) {
+    return 1;
+  } else {
+    return 0
+  }
 }
