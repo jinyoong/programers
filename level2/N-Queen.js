3204da676666c55dec327430b16fa6ae3345e044
jinyong <zhaqh146@naver.com>
2023-02-05 15:47:05 +0900

    [N-Queen] 재귀에 이차원 배열을 이용해서 해결

--- level2/N-Queen.js
+++ level2/N-Queen.js
@@ -1,39 +1,41 @@
 function solution(n) {
   let answer = 0;
 
-  function chess(before, column, idx, n) {
-    const current = new Array(n).fill(0);
-
+  function chess(board, idx, n) {
     if (idx === n) {
       answer += 1;
       return;
     };
   
     for (let c = 0; c < n; c++) {
-      if (column[c] === 1) {
-        continue;
-      };
+      if (board[idx][c] === 1) continue;
   
-      if ((c - 1) >= 0 && before[c - 1] === 1) {
-        current[c] = 1;
-        continue;
-      };
+      const copyBoard = new Array(n).fill(0).map((element, index) => [...board[index]]);
   
-      if (c + 1 < n && before[c + 1] === 1) {
-        current[c] = 1;
-        continue;
-      };
+      copyBoard[idx][c] = 1;
+      for (let i = 1; i < n; i++) {
+        const nr = idx + i;
+        const leftSide = c - i;
+        const rightSide = c + i;
   
-      current[c] = 1;
-      column[c] = 1;
-      chess(current, column, idx + 1, n)
-      current[c] = 0;
-      column[c] = 0;
+        if (nr >= n) break;
+        
+        copyBoard[nr][c] = 1;
+  
+        if (leftSide >= 0) {
+          copyBoard[nr][leftSide] = 1;
+        };
+  
+        if (rightSide < n) {
+          copyBoard[nr][rightSide] = 1;
+        };
+      };
+      chess(copyBoard, idx + 1, n);
     };
   };
 
-  chess(new Array(n).fill(0), new Array(n).fill(0), 0, n);
-
+  chess(new Array(n).fill(0).map(() => new Array(n).fill(0)), 0, n);
+  
   return answer;
 };
 
