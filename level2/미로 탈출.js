f886804cf17abef8bc8fa1cc14bf64da76a6e633
jinyong <zhaqh146@naver.com>
2023-02-21 19:12:35 +0900

    [미로 탈출] BFS 함수 분리

--- level2/미로 탈출.js
+++ level2/미로 탈출.js
@@ -1,95 +1,76 @@
+function bfs(start, end, maps, width, height) {
+  let result = -1;
+  let idx = 0;
+  const queue = [[...start, 0]];
+  const visited = new Set(start.join(':'));
+
+  while (idx < queue.length) {
+    const sr = queue[idx][0];
+    const sc = queue[idx][1];
+    const count = queue[idx][2];
+    idx += 1;
+
+    if (sr === end[0] && sc === end[1]) {
+      result = count;
+      break;
+    };
+
+    for (let d of [[-1, 0], [0, 1], [1, 0], [0, -1]]) {
+      const nr = sr + d[0];
+      const nc = sc + d[1];
+
+      if (nr < 0 || nr >= height || nc < 0 || nc >= width) {
+        continue;
+      };
+
+      if (visited.has(`${nr}:${nc}`)) {
+        continue;
+      };
+
+      if (maps[nr][nc] === 'X') {
+        continue;
+      };
+
+      queue.push([nr, nc, count + 1]);
+      visited.add(`${nr}:${nc}`);
+    };
+  };
+
+  return result;
+};
+
+
 function solution(maps) {
   let answer = 0;
   const width = maps[0].length;
   const height = maps.length;
-  const direction = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
+  let startPoint = [];
+  let leverPoint = [];
+  let endPoint = [];
   let LToECount = -1;
   let SToLCount = -1;
-  const LToE = [];
-  const visited = new Set();
 
   for (let r = 0; r < height; r++) {
     for (let c = 0; c < width; c++) {
-      const current = maps[r][c];
+      if (maps[r][c] === 'S') {
+        startPoint = [r, c];
+      };
 
-      if (current !== 'L') continue;
+      if (maps[r][c] === 'L') {
+        leverPoint = [r, c];
+      };
 
-      LToE.push([r, c, 0]);
-      visited.add([r, c].join(':'));
-      let idx = 0;
-
-      while (idx < LToE.length) {
-        const sr = LToE[idx][0];
-        const sc = LToE[idx][1];
-        const count = LToE[idx][2];
-        idx += 1;
-
-        if (maps[sr][sc] === 'E') {
-          LToECount = count;
-          break;
-        };
-
-        for (let d of direction) {
-          const nr = sr + d[0];
-          const nc = sc + d[1];
-
-          if (nr < 0 || nr >= height || nc < 0 || nc >= width) continue;
-
-          if (visited.has([nr, nc].join(':'))) continue;
-
-          if (maps[nr][nc] === 'X') continue;
-
-          LToE.push([nr, nc, count + 1]);
-          visited.add([nr, nc].join(':'));
-        };
+      if (maps[r][c] === 'E') {
+        endPoint = [r, c];
       };
     };
   };
+
+  SToLCount = bfs(startPoint, leverPoint, maps, width, height);
+  LToECount = bfs(leverPoint, endPoint, maps, width, height);
 
   if (LToECount === -1) return -1;
-
-  const SToL = [];
-  visited.clear();
-  
-
-  for (let r = 0; r < height; r++) {
-    for (let c = 0; c < width; c++) {
-      const current = maps[r][c];
-
-      if (current !== 'S') continue;
-
-      SToL.push([r, c, 0]);
-      visited.add([r, c].join(':'));
-      let idx = 0;
-
-      while (idx < SToL.length) {
-        const sr = SToL[idx][0];
-        const sc = SToL[idx][1];
-        const count = SToL[idx][2];
-        idx += 1;
-
-        if (maps[sr][sc] === 'L') {
-          SToLCount = count;
-          break;
-        };
-
-        for (let d of direction) {
-          const nr = sr + d[0];
-          const nc = sc + d[1];
-
-          if (nr < 0 || nr >= height || nc < 0 || nc >= width) continue;
-
-          if (visited.has([nr, nc].join(':'))) continue;
-
-          if (maps[nr][nc] === 'X') continue;
-
-          SToL.push([nr, nc, count + 1]);
-          visited.add([nr, nc].join(':'));
-        };
-      };
-    };
-  };
 
   if (SToLCount === -1) return -1;
 
