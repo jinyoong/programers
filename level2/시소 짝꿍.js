f6fe2fa4e5c3028ea48103e930cabf99ed19e53e
jinyong <zhaqh146@naver.com>
2023-02-25 19:42:20 +0900

    [시소 짝꿍] 조합을 이용 : 해결

--- level2/시소 짝꿍.js
+++ level2/시소 짝꿍.js
@@ -1,23 +1,36 @@
 function solution(weights) {
   let answer = 0;
-  let sortedWeights = weights.sort();
-  
-  for (let i = 0; i < sortedWeights.length - 1; i++) {
+  let array = new Array(901).fill(0);
+  let weightArray = new Array();
+
+  weights.forEach(weight => {
+    array[weight - 100] += 1;
+
+    if (array[weight - 100] === 1) {
+      weightArray.push(weight);
+    };
+  });
+
+  let sortedWeights = weightArray.sort((a, b) => a - b);
+
+  for (let i = 0; i < sortedWeights.length; i++) {
     const standard = sortedWeights[i];
+    const standardCount = array[standard - 100];
+
+    answer += parseInt(standardCount * (standardCount - 1) / 2);
 
     for (let j = i + 1; j < sortedWeights.length; j++) {
       const target = sortedWeights[j];
+      const targetCount = array[target - 100];
 
       if (target > standard * 2) break;
 
-      if (target === standard) answer += 1;
-      
-      if (target === standard * 2) answer += 1;
+      [[1, 2], [2, 3], [3, 4]].forEach(element => {
+        const x = element[0];
+        const y = element[1];
 
-      if (target * 2 === standard * 3) answer += 1;
-
-      if (target * 3 === standard * 4) answer += 1;
-
+        if (target * x === standard * y) answer += standardCount * targetCount
+      });
     };
   };
 
@@ -26,3 +39,9 @@ function solution(weights) {
 
 console.log(solution([100, 180, 360, 100, 270]));
 console.log(solution([100, 100, 100, 100]));
+console.log(solution([100, 100, 100, 200, 200]));
+console.log(solution([100, 100, 150, 200]));
+console.log(solution([100, 100]));
+console.log(solution([100, 100, 100, 200, 200, 200]));
+console.log(solution([100, 100, 100, 200, 200, 300, 400]));
+console.log(solution([100, 100, 100, 100, 100, 100, 100]));
