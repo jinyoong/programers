c9d216e4a482ef1201a8e922f972e5c0073556b8
jinyong <zhaqh146@naver.com>
2023-03-08 23:37:19 +0900

    [3 x n 타일링] mod 연산 적용 : 해결:

--- level2/3 x n 타일링.js
+++ level2/3 x n 타일링.js
@@ -5,17 +5,19 @@ function solution(n) {
     return answer;
   };
 
-  const dp = new Array(parseInt(n / 2) + 1).fill('0');
+  const dp = new Array(parseInt(n / 2) + 1).fill(0);
+  const MOD = 1000000007;
 
-  dp[0] = '1';
-  dp[1] = '3';
+  dp[0] = 1;
+  dp[1] = 3;
 
   for (let i = 2; i <= parseInt(n / 2); i++) {
-    dp[i] = String(Number(dp[i - 1] * 4) - Number(dp[i - 2]));
+    const modResult = (((dp[i - 1] * 4) % MOD) - (dp[i - 2] % MOD) + MOD) % MOD;
+    dp[i] = modResult;
   };
 
-  answer = Number(dp[parseInt(n / 2)]) % 1000000007;
+  answer = dp[parseInt(n / 2)];
   return answer;
 };
 
-console.log(solution(5000));
+console.log(solution(4000));
