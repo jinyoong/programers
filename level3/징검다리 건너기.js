7e2bbbe5c9f173215a1003f1bc66802fc247b709
jinyong <zhaqh146@naver.com>
2023-05-12 23:44:32 +0900

    [징검다리 건너기] 최대힙 구현 : 진행 중

--- level3/징검다리 건너기.js
+++ level3/징검다리 건너기.js
@@ -1,54 +1,72 @@
 function solution(stones, k) {
   let answer = 0;
-  let peoples = new Array(stones.length).fill(0);
+  let section = new Heap();
 
-  peoples[0] = stones[0];
-  stones[0] = 0;
-
-  for (let i = 1; i < stones.length + 1; i++) {
-
-    if (i === stones.length) {
-      for (let j = 1; j <= k; j++) {
-        const idx = i - j;
-        
-        if (idx < 0) {
-          break;
-        };
-
-        answer += peoples[idx];
-      };
-
-      break;
-    };
-
-    let maximum = stones[i];
-
-    for (let j = 1; j <= k; j++) {
-      const idx = i - j;
-
-      if (idx >= 0) {
-        const people = peoples[idx];
-        const possible = Math.min(people, maximum);
-
-        peoples[idx] -= possible;
-        maximum -= possible; 
-        peoples[i] += possible;
-      } else {
-        peoples[i] += maximum;
-        maximum = 0;
-      };
-
-      if (maximum === 0) {
-        break;
-      };
-    };
-
-    console.log(...peoples);
-    console.log(...stones);
-    console.log("=================")
+  for (let i = 0; i < stones.length; i++) {
+    const stone = stones[i];
+    section.heapPush(stone);
   };
 
   return answer;
+};
+
+class Heap {
+  constructor() {
+    this.heap = [0];
+  };
+
+  heapPush(x) {
+    this.heap.push(x);
+    let childIdx = this.heap.length - 1;
+    let childNum = this.heap[childIdx];
+
+    while(childIdx > 1) {
+      const parentIdx = parseInt(childIdx / 2);
+      const parentNum = this.heap[parentIdx];
+
+      if (parentNum < childNum) {
+        this.heap[childIdx] = parentNum;
+        this.heap[parentIdx] = childNum;
+        childIdx = parentIdx;
+        childNum = parentNum;
+      } else {
+        break;
+      };
+    };
+  };
+
+  heapPop() {
+    const result = this.heap[1];
+    let lastIdx = this.heap.length - 1;
+    let parentIdx = 1;
+
+    this.heap[1] = this.heap[lastIdx];
+    this.heap.pop();
+    lastIdx -= 1;
+
+    while(parentIdx * 2 <= lastIdx) {
+      const parent = this.heap[parentIdx];
+      let childIdx = parentIdx * 2;
+      let child = this.heap[childIdx];
+      
+      if (2 * parentIdx + 1 <= lastIdx) {
+        const rightChild = this.heap[2 * parentIdx + 1];
+
+        if (child < rightChild) {
+          child = rightChild;
+          childIdx += 1;
+        };
+      };
+
+      if (parent < child) {
+        this.heap[parentIdx] = child;
+        this.heap[childIdx] = parent;
+        parentIdx = childIdx;
+      };
+    };
+
+    return result
+  };
 };
 
 console.log(solution([2, 4, 5, 3, 2, 1, 4, 2, 5, 1], 3));
