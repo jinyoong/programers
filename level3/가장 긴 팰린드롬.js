eb1ec84d32c5452651b97ead187268eaecace4f9
jinyong <zhaqh146@naver.com>
2023-05-16 23:46:31 +0900

    [가장 긴 팰린드롬] DP : 해결

--- level3/가장 긴 팰린드롬.js
+++ level3/가장 긴 팰린드롬.js
@@ -1,29 +1,30 @@
 function solution(s) {
   let answer = 1;
+  let wordLength = s.length;
+  let dp = new Array(s.length).fill(0).map(() => new Array(s.length).fill(false));
+  // dp[i][j] : i번째 글자부터 j번째 글짜까지가 팰린드롬인지 여부
 
-  for (let i = s.length; i > 1; i--) {
-    for (let j = 0; j <= s.length - i; j++) {
-      if (isPalindrome(s, i, j)) {
-        answer = i;
-        return answer;
+  for (let i = 0; i < wordLength; i++) {
+    dp[i][i] = true;
+  };
+
+  for (let i = 0; i < wordLength - 1; i++) {
+    if (s[i] === s[i + 1]) {
+      dp[i][i + 1] = true;
+    };
+  };
+
+  for (let interval = 3; interval < wordLength + 1; interval++) {
+    for (let start = 0; start < wordLength - interval + 1; start++) {
+      const end = start + interval - 1;
+      if (s[start] === s[end] && dp[start + 1][end - 1]) {
+        dp[start][end] = true;
+        answer = interval;
       };
     };
   };
 
   return answer;
-};
-
-function isPalindrome(word, targetLength, start) {
-  let result = true;
-
-  for (i = 0; i < parseInt(targetLength / 2); i++) {
-    if (word[start + i] !== word[start + targetLength - i - 1]) {
-      result = false;
-      break;
-    };
-  };
-
-  return result;
 };
 
 console.log(solution("abcdcba"));
