e82d62f4510d1e1fff405a6e10d68413c4c2f7ee
jinyong <zhaqh146@naver.com>
2023-06-12 22:27:33 +0900

    [파트 2] 2-5. GNB 메뉴 클릭 시, URL 주소 변경 후 페이지 렌더링하기 구현

--- assignments_spa/src/Header.js
+++ assignments_spa/src/Header.js
@@ -16,10 +16,17 @@ class Header {
     return div;
   }
 
+  changeUrl(url) {
+    window.history.pushState('', '', url);
+  }
+
   render() {
     const header = document.createElement('header');
     const homeMenu = this.makeHeaderMenu('header header_left', 'menu_home', 'menu_name', 'HOME')
+    homeMenu.addEventListener('click', () => this.changeUrl('/web/'))
+
     const signupMenu = this.makeHeaderMenu('header header_right', 'menu_signup', 'menu_name', 'SIGNUP')
+    signupMenu.addEventListener('click', () => this.changeUrl('/web/signup/'))
 
     header.appendChild(homeMenu);
     header.appendChild(signupMenu);
